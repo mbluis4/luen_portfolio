f256eb0b5d480b62dd956b34beb8176e4be7e600
mbluis4 <92177912+mbluis4@users.noreply.github.com>
2022-04-20 22:44:32 -0300

    fixed error with about me button

--- src/components/Project.js
+++ src/components/Project.js
@@ -15,7 +15,7 @@ const Project = ({ project }) => {
         ></img>
       </div>
       <a href={url} target="_blank">
-        <FaExternalLinkAlt className="-mt-3 text-md" />
+        <FaExternalLinkAlt className="-mt-3 text-md scale-100 hover:scale-125 transition-all" />
       </a>
       <h2 className="text-xl tracking-wide my-2">{name}</h2>
       <div className="flex space-x-2">
@@ -28,9 +28,16 @@ const Project = ({ project }) => {
           </div>
         ))}
       </div>
-      <div className="bg-teal-500 w-full rounded-sm text-right mt-2 p-1 text-slate-50 flex items-center space-x-2 justify-end">
+      <div
+        className="bg-teal-500 w-full rounded-sm text-right 
+      mt-2 p-1 text-slate-50 flex items-center space-x-2 justify-end"
+      >
         <FaGithub />
-        <a href={github} target="_blank">
+        <a
+          href={github}
+          target="_blank"
+          className="scale-100 hover:scale-105 transition-all"
+        >
           Source code
         </a>
       </div>
